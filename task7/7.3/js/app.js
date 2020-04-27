fe48ff31e5f0562a5abf201014da77e9ba3403d8
Andrey Shelemetev <andrey.shelemetev@gmail.com>
2020-04-27 23:44:32 +0300

    add task7

--- task7/7.3/js/app.js
+++ task7/7.3/js/app.js
@@ -1,7 +1,7 @@
 var titleText = 'Привет, мой друг! Добро пожаловать на сайт группы отелей Selly Hotels!';
 
 var promoTitle = document.getElementById('promoTitle'); // получаем заголовок страницы
-promoTitle.innerText = titleText;                       // заменяем текст в заголовке
+promoTitle.innerText = titleText; // заменяем текст в заголовке
 
 var button = document.getElementById('showAllFeedbacks');
 button.addEventListener('click', showAllFeedbacks);
