b88e4f797957bc072271effe360b4effbbf52dcf
Andrey Shelemetev <andrey.shelemetev@gmail.com>
2020-05-14 22:27:23 +0300

    add task8 task8.3

--- task8/8.3/js/app.js
+++ task8/8.3/js/app.js
@@ -1,19 +1,31 @@
-let icon = document.querySelector(".icon")
-let topmenu = document.getElementById("topmenu")
+let icon = $('#js_icon')
+let topmenu = $('#js_topmenu')
 let links = $('.menu_link')
+let create_button = $('#js_create_button')
 //console.log(links)
 
-icon.addEventListener("click", function (e) {
-    icon.classList.toggle("ac");
-    topmenu.classList.toggle("hidden");
+$(icon).on('click', function() {
+    icon.toggleClass("ac");
+    topmenu.toggleClass("hidden");
+    icon.toggleClass("icon_fixed");
 })
 
 for (let i = 0; i< links.length; i++) {
     let link = links[i];
     $(link).mouseover(function() {
-        $(link).css("color", "black");;
+        $(link).css("color", "black");
     });
         $(link).mouseout(function() {
-        $(link).css("color", "white");;
+        $(link).css("color", "white");
     });
 }
+
+    $(create_button).mouseover(function() {
+        $(create_button).css("color", "black");
+        $(create_button).css("border", "solid 3px #fff");
+    });
+
+    $(create_button).mouseout(function() {
+        $(create_button).css("color", "white");
+        $(create_button).css("border", "solid 3px #000");
+    });
